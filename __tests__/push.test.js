d574e07dbb539c9657ccb6af951519c48dea9c49
John Ellis <john@deckerego.net>
2021-03-03 16:26:06 -0500

    Working tests

--- __tests__/push.test.js
+++ __tests__/push.test.js
@@ -1,10 +1,30 @@
 const { get } = require("../handlers/push");
+const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
+
+jest.mock("@aws-sdk/client-sqs", () => {
+  const mockSQSClient = jest.fn().mockImplementation(() => {
+    return { send: (message) => message };
+  });
+  const mockSendMessageCommand = jest.fn().mockImplementation((payload) => {
+    return payload;
+  });
+  return {
+    SQSClient: mockSQSClient,
+    SendMessageCommand: mockSendMessageCommand
+  };
+});
 
 describe("push", () => {
-  test("Receive HTTP GET", async () => {
-    const event = { testKey: "testValue" };
-    const response = await get(event, {});
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test("Receive empty HTTP GET", async () => {
+    const response = await get({}, {});
     expect(response.statusCode).toBe(200);
-    expect(response.body).toBe("{\"testKey\":\"testValue\"}");
+    expect(JSON.parse(response.body)).toEqual({
+      DelaySeconds: 0,
+      MessageAttributes:{}
+    });
   });
 });
--- handlers/push.js
+++ handlers/push.js
@@ -1,8 +1,41 @@
 'use strict';
 
+const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
+const SQS = new SQSClient({
+  region: process.env.AWS_REGION
+});
+
 module.exports.get = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(event)
-  };
+  try {
+    const payload = toMessage(event, process.env.SQS_HTTP_ARN);
+    const messageCommand = new SendMessageCommand(payload);
+    const acknowledgement = await SQS.send(messageCommand);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(acknowledgement)
+    };
+  } catch(exception) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(exception)
+    };
+  }
 };
+
+function toMessage(httpRequest, queueUrl) {
+  return {
+    DelaySeconds: 0,
+    MessageAttributes: {
+      Resource: httpRequest.resource,
+      Path: httpRequest.path,
+      Method: httpRequest.httpMethod,
+      Headers: httpRequest.headers,
+      MultiValueHeaders: httpRequest.multiValueHeaders,
+      QueryStringParameters: httpRequest.queryStringParameters,
+      MultiValueQueryStringParameters: httpRequest.multiValueQueryStringParameters,
+      PathParameters: httpRequest.pathParameters
+    },
+    MessageBody: httpRequest.body,
+    QueueUrl: queueUrl
+  };
+}
